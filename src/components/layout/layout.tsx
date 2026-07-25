c6a39c281060da3074cecac4471c9a6fef41ef45
Kacper Bobrowski <bober933@gmail.com>
2026-07-25 21:24:24 +0200

    feat: add header (#29)
    - add save to png functionality

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,17 @@
 import type { ReactNode } from 'react';
 
+import { Header } from '../header';
 import styles from './layout.module.css';
 
 interface LayoutProps {
   children: ReactNode;
+  onExportMap?: () => void;
 }
 
-export function Layout({ children }: LayoutProps) {
+export function Layout({ children, onExportMap }: LayoutProps) {
   return (
     <div className={styles.layout}>
+      <Header onExportMap={onExportMap} />
       <main className={styles.main}>{children}</main>
     </div>
   );
